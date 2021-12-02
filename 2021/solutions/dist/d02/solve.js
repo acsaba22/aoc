c8a51da79fbe1a15f61b3e489a3aa83c2353a979
Csaba Andras <acsaba@gmail.com>
2021-12-02 16:58:59 +0100

    day 02

---
+++ 2021/solutions/dist/d02/solve.js
@@ -0,0 +1,38 @@
+function p1(lines) {
+    let x = 0;
+    let y = 0;
+    const move = new Map([
+        ['forward', (n) => x += n],
+        ['down', (n) => y += n],
+        ['up', (n) => y -= n],
+    ]);
+    for (const l of lines) {
+        const [d, n] = l.split(' ');
+        move.get(d)(Number(n));
+    }
+    log('P1: ', x * y); // 1882980
+}
+function p2(lines) {
+    let x = 0;
+    let y = 0;
+    let aim = 0;
+    let move = new Map([
+        ['forward', (n) => {
+                x += n;
+                y += n * aim;
+            }],
+        ['down', (n) => aim += n],
+        ['up', (n) => aim -= n],
+    ]);
+    for (let l of lines) {
+        let [d, n] = l.split(' ');
+        move.get(d)(Number(n));
+    }
+    log('P2: ', x * y); // 1882980
+}
+export async function main() {
+    let s = await loadFile('/src/d02/input.txt');
+    let lines = s.trim().split('\n');
+    p1(lines);
+    p2(lines);
+}
---
+++ 2021/solutions/src/d02/solve.ts
@@ -0,0 +1,47 @@
+import { type } from "os"
+
+declare function log(...args: any[]) : void;
+declare function loadFile(fname: string): Promise<string>
+
+type fuggveny = (n:number) => void
+
+function p1(lines: string[]) {
+  let x = 0
+  let y = 0
+  const move = new Map([
+    ['forward', (n : number) => x += n],
+    ['down', (n : number) => y += n],
+    ['up', (n : number) => y -= n],
+  ])
+  for (const l of lines) {
+    const [d, n] = l.split(' ');
+    move.get(d)!(Number(n))
+  }
+  log('P1: ', x * y) // 1882980
+}
+
+function p2(lines: string[]) {
+  let x = 0
+  let y = 0
+  let aim = 0
+  let move = new Map([
+    ['forward', (n : number) => {
+      x += n
+      y += n*aim
+    }],
+    ['down', (n : number) => aim += n],
+    ['up', (n : number) => aim -= n],
+  ])
+  for (let l of lines) {
+    let [d, n] = l.split(' ')
+    move.get(d)!(Number(n))
+  }
+  log('P2: ', x * y) // 1882980
+}
+
+export async function main() {
+  let s: string = await loadFile('/src/d02/input.txt')
+  let lines = s.trim().split('\n')
+  p1(lines)
+  p2(lines)
+}
