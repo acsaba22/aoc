671995488b8fea7ee77933fb06eeaa392ab4ecb6
Csaba Andras <acsaba@gmail.com>
2025-12-08 15:41:23 +0100

    2025 d03

---
+++ 2025/d03/Main.hs
@@ -0,0 +1,23 @@
+module Main (main) where
+
+joltage :: Int -> String -> String
+joltage 0 _ = ""
+joltage digits s = ret
+  where
+    -- TODO is this O(n)
+    k = length s - digits + 1
+    -- TODO better way to find first?
+    (maxv, maxp) = maximum $ zip (take k s) (map (* (-1)) [0 :: Int ..])
+    ret = [maxv] ++ joltage (digits - 1) (drop (-maxp + 1) s)
+
+main :: IO ()
+main = do
+  input <- lines <$> readFile "d03/input.txt"
+  let solve k = (sum $ map read $ map (joltage k) input) :: Int
+  let p1 = solve 2
+  let p2 = solve 12
+  putStrLn $ "!!!!!!!!! P1: " ++ show p1
+  putStrLn $ "!!!!!!!!! P2: " ++ show p2
+
+-- !!!!!!!!! P1: 17403
+-- !!!!!!!!! P2: 173416889848394
