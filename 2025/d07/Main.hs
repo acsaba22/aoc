16ac619f46c9f885aa0a54398a2421163112076d
Csaba Andras <acsaba@gmail.com>
2025-12-13 12:58:33 +0100

    2025 d07 p2

--- 2025/d07/Main.hs
+++ 2025/d07/Main.hs
@@ -1,64 +1,72 @@
 module Main (main) where
-import qualified Data.ByteString.Char8 as BS
+import Data.Vector (Vector, (!), (!?))
+import qualified Data.Vector as V -- TODO V.length -et lehet mashogy is importalni?
 
-type ByteString = BS.ByteString
+fname :: String
+fname = "d07/example.txt"
 
-indexDefault :: ByteString -> Int -> Char
+type CI = (Char, Int)
+
+toCI :: Char -> CI
+toCI 'S' = ('|', 1)
+toCI c = (c, 0)
+
+type Vec = Vector CI
+
+toPrint :: Vec -> String
+toPrint v = map fst $ V.toList v
+
+toPrintAll :: [Vec] -> String
+toPrintAll vv = unlines $ map toPrint vv
+
+indexDefault :: Vec -> Int -> CI
 indexDefault bs i = case maybeVal of 
     Just c -> c
-    _ -> '.'
+    _ -> ('.', 0)
   where
-    maybeVal = bs `BS.indexMaybe` i
+    maybeVal = bs !? i
 
 
 -- TODO kiszamoltam az uj terkepet, megneztem az aljat, 9 volt de rajottem 
 -- hogy az kelett volna hogy hany split volt
 -- mas nyelvben olyan konnyu lenne egy kis side data-t kipakolni, itt hogy csinaljam?
 
-propagate :: ByteString -> ByteString -> ByteString
+propagate :: Vec -> Vec -> Vec
 propagate upLine oldState = newState
   where
-    newStateS = [newVal i | i <- [0.. (BS.length oldState)-1]]
+    newStateS = [newVal i | i <- [0.. (V.length oldState)-1]]
       where
         newVal i = newChar
           where
-            old = oldState `BS.index` i
-            up = upLine `BS.index` i
+            old = oldState ! i
+            up = upLine ! i
             left = oldState `indexDefault` (i-1)
             leftUp = upLine `indexDefault` (i-1)
             right = oldState `indexDefault` (i+1)
             rightUp = upLine `indexDefault` (i+1)
-            newChar = case old of
-              '^' -> '^'
-              '.' -> if (
-                up == 'S' ||
-                up == '|' ||
-                (left == '^' && leftUp == '|') ||
-                (right == '^' && rightUp == '|')) 
-                then '|' else '.'
+            timelines = (
+              snd up +
+              (if fst left == '^' then snd leftUp else 0) +
+              (if fst right == '^' then snd rightUp else 0))
+            
+            newChar = case fst old of
+              '^' -> ('^', 0)
+              '.' -> ((if timelines == 0 then '.' else '|'), timelines)
               _ -> error "bad char"
         
-    newState = BS.pack newStateS
+    newState = V.fromList newStateS
 
 -- TODO this is like foldl or foldl' but keeps list, probably exists
-flowDown :: ByteString -> [ByteString] -> [ByteString]
+flowDown :: Vec -> [Vec] -> [Vec]
 flowDown upLine (oldState:oldStates) = nextLine : flowDown nextLine oldStates
   where
     nextLine = propagate upLine oldState
 flowDown _ [] = []
 
-(!!!) :: BS.ByteString -> Int -> Char
-(!!!) = BS.index
-
-splitCount :: (ByteString, ByteString) -> Int
-splitCount (up, down) = sum [1 | i <- [0.. (BS.length up) - 1] , isSplit i]
+splitCount :: (Vec, Vec) -> Int
+splitCount (up, down) = sum [1 | i <- [0.. (V.length up) - 1] , isSplit i]
   where
-    isSplit i = (up !!! i) == '|' && (down !!! i) == '^'
-
--- TODO make it a template
--- pairs :: [a] -> [(a,a)]
--- pairs :: [ByteString] -> [(ByteString,ByteString)]
--- pairs (x:y:xs) = (x, y) : (y:xs)
+    isSplit i = fst (up ! i) == '|' && fst (down ! i) == '^'
 
 pairs :: [a] -> [(a,a)]
 pairs (x:y:xs) = (x, y) : pairs (y:xs)
@@ -66,20 +74,16 @@ pairs _ = []
 
 main :: IO ()
 main = do
-  input <- BS.lines <$> BS.readFile "d07/input.txt"
-  mapM_ print input
+  input <- lines <$> readFile fname
+  let inputV = map V.fromList $ map (map toCI) input :: [Vec]
+  putStrLn $ toPrintAll inputV
 
-  print "**************************"
+  putStrLn "**************************"
 
-  let p1Field = flowDown (head input) (tail input)
-  mapM_ print p1Field
+  let p1Field = flowDown (head inputV) (tail inputV)
+  putStrLn $ toPrintAll p1Field
 
-  -- let ps = pairs p1Field :: [(ByteString, ByteString)] 
-  -- let s = map splitCount ps :: [Int]
-  -- print $ sum $ map splitCount $ pairs p1Field
-
-  -- print $ BS.count '|' $ last p1Field
   let p1 = sum $ map splitCount $ pairs p1Field
-  let p2 = 0 :: Int
+  let p2 = sum $ V.map snd $ last p1Field :: Int
   putStrLn $ "!!!!!!!!! P1: " ++ show p1
   putStrLn $ "!!!!!!!!! P2: " ++ show p2
